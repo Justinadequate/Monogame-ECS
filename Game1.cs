949dfed03eb92125f829de0dccac729cbc685c08
Justinadequate <56033937+Justinadequate@users.noreply.github.com>
2021-07-09 22:10:32 -0400

    attempt at importing a tiled map

--- Game1.cs
+++ Game1.cs
@@ -1,24 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using Monogame1.ECS;
-using Monogame1.ECS.Components;
 using Monogame1.ECS.Factories;
-using Monogame1.ECS.Models;
 using Monogame1.ECS.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace Monogame1
 {
     public class Game1 : Game
     {
+        // TODO: cannot import tiled map and pipeline dll not in build folder !!!!
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Drawing_System _drawSystem;
         private Animation_System _animationSystem;
         private PlayerControl_System _playerSystem;
         private Physics_System _physicsSystem;
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
@@ -34,6 +37,8 @@ namespace Monogame1
 
         protected override void LoadContent()
         {
+            _tiledMap = Content.Load<TiledMap>("test1");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             new EntityManager();
             
@@ -44,10 +49,10 @@ namespace Monogame1
 
             SkeletonFactory.CreateSkeleton(Content, 1);
 
-            Entity floor1 = new Entity("Floor1");
-            floor1.AddComponent(new Transform(new Vector2(0, (_graphics.GraphicsDevice.Viewport.Height / 3) * 2)));
-            floor1.AddComponent(new Rendering(Content.Load<Texture2D>("FloorA/spr_PisoA_strip18")));
-            floor1.AddComponent(new Collider());
+            // Entity floor1 = new Entity("Floor1");
+            // floor1.AddComponent(new Transform(new Vector2(0, (_graphics.GraphicsDevice.Viewport.Height / 3) * 2)));
+            // floor1.AddComponent(new Rendering(Content.Load<Texture2D>("FloorA/spr_PisoA_strip18")));
+            // floor1.AddComponent(new Collider());
         }
 
         protected override void Draw(GameTime gameTime)
@@ -55,6 +60,7 @@ namespace Monogame1
             GraphicsDevice.Clear(Color.Transparent);
 
             _spriteBatch.Begin();
+            _tiledMapRenderer.Draw();
             _drawSystem.Draw(_spriteBatch);
             _spriteBatch.End();
 
@@ -67,6 +73,7 @@ namespace Monogame1
                 Exit();
             float deltaTime = ((float)gameTime.ElapsedGameTime.Milliseconds) / 1000f;
 
+            _tiledMapRenderer.Update(gameTime);
             _animationSystem.Update(deltaTime);
             _playerSystem.Update();
             _physicsSystem.Update();
